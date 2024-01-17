b99048826a94d96b30fba7a7396d7fd72b1248fc
Pavel2232 <pavelmazikinvladimirovich@gmail.com>
2024-01-17 22:51:24 +0300

    Add functional listen chat and write in file in Queue

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import asyncio
+import datetime
 import time
+
+import aiofiles
 
 import gui
 from socket_context_manager import Socket
+
+
+async def save_messages(message, queue):
+    async with aiofiles.open('boss.txt', mode='a') as f:
+        await queue.put(await f.write(message))
 
 
 async def generate_msgs(queue):
@@ -12,17 +20,18 @@ async def generate_msgs(queue):
     return None
 
 # async def read_msgs(host, port, queue):
-async def read_msgs(queue):
+async def read_msgs(queue, queue_write):
 
     socket = Socket('minechat.dvmn.org', 5000)
 
     while True:
         async with socket:
             data_chanel = await socket.reader.readline()
-            # date = f'[{datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}] '
-            log_message = data_chanel.decode()
-            # await write_message_file(log_message, history_file=history_file)
-            await queue.put(log_message)
+            date = f'[{datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")}] '
+            message = data_chanel.decode()
+            log_message = date + message
+            await save_messages(log_message, queue_write)
+            await queue.put(message)
 
 
 
@@ -32,11 +41,14 @@ async def main():
     messages_queue = asyncio.Queue()
     # messages_queue.put_nowait('''Привет обитателям чата!
     # Как дела?''')
-
+    write_file_queue = asyncio.Queue()
     sending_queue = asyncio.Queue()
     status_updates_queue = asyncio.Queue()
     async with asyncio.TaskGroup() as tg:
-        tg.create_task(read_msgs(messages_queue))
+        async with aiofiles.open('boss.txt', mode='r') as f:
+            messages_queue.put_nowait(await f.read())
+        tg.create_task(read_msgs(messages_queue, write_file_queue))
+
 
         return await gui.draw(messages_queue, sending_queue, status_updates_queue)
 
